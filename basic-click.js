54d52cbcc8a929af4348936889ad65d0fdfae5a0
RafaPolit <rafaelpolit@gmail.com>
2023-05-11 19:52:04 -0500

    Changed pin number

--- basic-click.js
+++ basic-click.js
@@ -4,12 +4,12 @@ const Gpio = require("onoff").Gpio; // Gpio class
 
 // Export GPIO4 as an interrupt generating input with a debounceTimeout of 10
 // milliseconds
-const button = new Gpio(3, "in", "rising", { debounceTimeout: 10 });
+const button = new Gpio(22, "in", "rising", { debounceTimeout: 10 });
 
-console.log("Please press the button on GPIO3...");
+console.log("Please press the button on GPIO22...");
 
 // The callback passed to watch will be invoked when the button connected to
-// GPIO3 is pressed
+// GPIO22 is pressed
 button.watch((err, value) => {
   if (err) {
     throw err;
