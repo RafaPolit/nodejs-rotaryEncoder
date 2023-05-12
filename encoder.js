b1eddcc78a99f0868de1b916bcc39f52bd3f344a
RafaPolit <rafaelpolit@gmail.com>
2023-05-11 21:49:39 -0500

    Long press added

--- encoder.js
+++ encoder.js
@@ -1,19 +1,24 @@
 "use strict";
+const { hrtime } = require("node:process");
 
 const Gpio = require("onoff").Gpio; // Gpio class
 
 const clk = new Gpio(17, "in", "rising");
 const dt = new Gpio(27, "in", "both");
-const sw = new Gpio(22, "in", "rising", { debounceTimeout: 10 });
+const sw = new Gpio(22, "in", "both", { debounceTimeout: 10 });
 
 console.log("Rotate or click on the encoder");
 
 let rotation = 0;
 let click = 0;
+let longClick = 0;
+let hrStart = 0;
 
 const formatOutput = () => {
   process.stdout.clearLine();
-  process.stdout.write(`Rotation: ${rotation}, Click: ${click}\r`);
+  process.stdout.write(
+    `Rotation: ${rotation}, Click: ${click}, Long Click: ${longClick}\r`
+  );
 };
 
 clk.watch((err, clkValue) => {
@@ -36,7 +41,17 @@ sw.watch((err, value) => {
     throw err;
   }
 
-  click += 1;
+  if (value === 1) {
+    hrStart = hrtime.bigint();
+  } else {
+    const hrEnd = hrtime.bigint();
+    console.log("took:", hrEnd - hrStart);
+    if (hrEnd - hrStart > 1000000) {
+      longClick += 1;
+    } else {
+      click += 1;
+    }
+  }
   formatOutput();
 });
 
