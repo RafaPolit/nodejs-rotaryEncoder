52261da218e22ec117a85a44cbe9250c9ddc47ec
RafaPolit <rafaelpolit@gmail.com>
2023-05-11 21:08:34 -0500

    First encoder test

---
+++ encoder.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const Gpio = require("onoff").Gpio; // Gpio class
+
+const clk = new Gpio(17, "in", "rising", { debounceTimeout: 10 });
+const dt = new Gpio(27, "in", "both", { debounceTimeout: 10 });
+const sw = new Gpio(22, "in", "rising", { debounceTimeout: 10 });
+
+console.log("Rotate or click on the encoder");
+
+clk.watch((err, value) => {
+  if (dt.readSync() === value) {
+    console.log("CW");
+  } else {
+    console.log("CCW");
+  }
+});
+
+sw.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log("Button pressed!, its value was " + value);
+});
+
+process.on("SIGINT", (_) => {
+  clk.unexport();
+  dt.unexport();
+  sw.unexport();
+});
